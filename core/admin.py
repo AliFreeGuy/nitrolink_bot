bc74be0cbb587552129a01041283e66a96a053a6
AliFreeGuy <alifreeguy@gmail.com>
2024-03-01 11:47:34 +0330

    feat: create bot model and add to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import BotModel
 
-# Register your models here.
+from django.contrib import admin
+from core.models import BotModel
+
+class BotModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'username', 'token')
+    search_fields = ('name', 'username', 'token')
+    ordering = ('name',)
+
+admin.site.register(BotModel, BotModelAdmin)
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+class BotModel(models.Model):
+    name = models.CharField(max_length = 128 , unique = True)
+    username = models.CharField(max_length = 128 , unique = True)
+    token = models.CharField(max_length= 128 , unique = True)
+    api_hash = models.CharField(max_length = 225)
+    api_id = models.CharField(max_length = 225)
+
+    def __str__(self) -> str:
+        return str(self.name)
+    
+
+
+    
+
+    class Meta :
+
+        verbose_name = "My Bots"
+        verbose_name_plural = "My Bots"
