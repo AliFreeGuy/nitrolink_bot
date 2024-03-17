c80fffceca0beffc7a86b4ff86dc0cfaf04012e0
AliFreeGuy <alifreeguy@gmail.com>
2024-03-17 10:27:23 +0330

    fix: fix send message activation sub and new sub

--- file_to_link/signals/free_sub.py
+++ file_to_link/signals/free_sub.py
@@ -15,12 +15,13 @@ from core.models import BotModel
 def add_free_sub_for_new_user(sender, instance, created, **kwargs):
     if created:
         free_sub = FileToLinkPlansModel.objects.filter(tag = 'free').first()
-
+        print(free_sub)
         if free_sub :
             try :
 
                 user = instance 
-                bot = BotModel.objects.filter(tag = 'file_to_link').first()
+                bot = BotModel.objects.filter(name = 'file_to_link').first()
                 data = UserPlanModel.objects.create(bot = bot , user = user , plan = free_sub)
+                print('activation sub new user ')
             except Exception as e :
                 print(f'ERROR: {str(e)}')
--- file_to_link/utils/telegram_send_message.py
+++ file_to_link/utils/telegram_send_message.py
@@ -21,8 +21,4 @@ def send_message(chat_id , message, bot_token ):
 
 
 
-def edit_message(chat_id, message_id, text, bot_token):
-    url = f'https://api.telegram.org/bot{bot_token}/editMessageText'
-    payload = {'chat_id': chat_id, 'message_id': message_id, 'text': text}
-    response = requests.post(url, data=payload)
 
